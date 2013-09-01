2e1a167d124011216ad184389860f7083a47c497
Kristian Nielsen <knielsen@knielsen-hq.org>
2013-09-01 22:32:58 +0200

    Add simple PWM test.

--- main.c
+++ main.c
@@ -96,6 +96,49 @@ setup_gpio(void)
 
 
 static void
+setup_timer(void)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+  TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+  TIM_OCInitTypeDef TIM_OCInitStructure;
+  uint32_t period;
+
+  period = 84000000/50000-1;
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5, ENABLE);
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource0, GPIO_AF_TIM5);
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource1, GPIO_AF_TIM5);
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_TIM5);
+  GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_TIM5);
+
+  TIM_TimeBaseStructure.TIM_Period = period;
+  TIM_TimeBaseStructure.TIM_Prescaler = 0;
+  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+  TIM_TimeBaseInit(TIM5, &TIM_TimeBaseStructure);
+
+  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = period/40;
+  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+  TIM_OC1Init(TIM5, &TIM_OCInitStructure);
+
+  TIM_OC1PreloadConfig(TIM5, TIM_OCPreload_Enable);
+  TIM_ARRPreloadConfig(TIM5, ENABLE);
+  TIM_Cmd(TIM5, ENABLE);
+}
+
+
+static void
 setup_led(void)
 {
   GPIO_InitTypeDef GPIO_InitStructure;
@@ -319,6 +362,7 @@ int main(void)
   setup_serial();
   setup_gpio();
   setup_led();
+  setup_timer();
   serial_puts(USART1, "Initialising...\r\n");
   set_channel(1, 0);
   set_channel(2, 0);
